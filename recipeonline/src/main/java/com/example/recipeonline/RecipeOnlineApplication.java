0505f32b4c76368fc1a1709a9ea53418c01fecf3
ThienLy16 <thienlv16@gmail.com>
2020-05-20 00:49:41 +0700

    create sring boot properties

---
+++ recipeonline/src/main/java/com/example/recipeonline/RecipeOnlineApplication.java
@@ -0,0 +1,14 @@
+package com.example.recipeonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeOnlineApplication.class, args);
+		System.out.println("done");
+	}
+
+}
